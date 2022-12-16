98570413925a72a04e5db1f49544d7ee7ff4dfbb
andrewphilipsmith <andrewphilipsmith@users.noreply.github.com>
2022-12-16 15:53:10 +0000

    Apply friendly names to TalkLife CMOC methods

--- src/data/tl_generation_wrapper.py
+++ src/data/tl_generation_wrapper.py
@@ -5,19 +5,33 @@ from icecream import ic
 
 from .source import Source
 
-TL_GENERATION_PATH = Path("../../../anthony/timeline_generation").absolute().resolve()
+TL_GENERATION_PATH = Path("../anthony/timeline_generation").absolute().resolve()
 
 ic(TL_GENERATION_PATH)
 sys.path.append(str(TL_GENERATION_PATH))
 
 try:
     from utils.io.my_pickler import my_pickler
+    from utils.visualize.plot_results import method_name_mapper
     data_daily_interactions = my_pickler(
         "i", "observed_data_daily_interactions", folder="datasets"
     )
 
     all_cmocs = my_pickler("i", "candidate_moments_of_change", folder="datasets")
 
-    talk_life_aggregate = Source("talklife-aggregated", data_daily_interactions, all_cmocs)
-except:
-    pass
+    _source = Source("talklife-aggregated", data_daily_interactions, all_cmocs)
+    # _source.cmoc_method_friendly_names = lambda n:
+
+    def method_name_wrapper(method_id: str) -> str:
+        try:
+            return method_name_mapper([method_id])[method_id]
+        except KeyError:
+            return method_id.replace("_", " ").title()
+
+    ic(_source.cmoc_methods)
+
+    _source.cmoc_method_friendly_names = method_name_wrapper
+    talk_life_aggregate = _source
+
+except Exception as ex:
+    print(ex)
